2b64aeef78cab729c76158440c8f69e043178a0b
Henri Einroos <henri.einroos@gmail.com>
2023-11-13 01:54:21 +0200

    mängu lõpp check, algus+chall activation halb

---
+++ mängututvustus.py
@@ -0,0 +1,22 @@
+# mängututvustus.py
+
+def kuvage_juhendid():
+    return """
+    Tere tulemast mängu "Joomasõbrad" - Ultimaatne Joomise Seiklus!
+
+    Astuge teekonnale täis naeru, sõprust ja aimu kelmikusest. "Joomasõbrad" ei ole lihtsalt lauamäng; see on pass meeldejäävateks hetkedeks ja lõputuks lõbuks.
+
+    Kui teete täringut ja liigute laual, kohtate väljakutseid, mis on inspireeritud klassikalistest peomängudest nagu "Mina kunagi pole" ja "Tõde või Tegu". Valmistuge ootamatuteks pööreteks ja naljakateks paljastusteks!
+
+    Juhised:
+    - Klikkige mängulaual, et käivitada see epohhiline seiklus.
+    - Veeretage täringut, liikuge oma märgiga ja võtke vastu teid ees ootavad väljakutsed.
+    - Võidate, jõudes ihaldatud "FINISH" ruuduni, kuid mäletage, et tegelik võit peitub jagatud hetkedes ja mängu rõõmus.
+
+    Kutsuge kokku oma sõbrad, valage oma lemmikjoogid ja alustage mänge! Kas olete valmis "Joomasõbrad" kogemuseks?
+
+    Klõpsake 's', et alustada mängu ja laske naerul voolata!
+    """
+
+if __name__ == "__main__":
+    print(kuvage_juhendid())
