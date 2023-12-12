4c13919d9a63c75858f574a605e866d1db0c6c56
grete <armei.grete@gmail.com>
2023-12-12 12:18:12 +0200

    blabla

--- grete_prrov.py
+++ grete_prrov.py
@@ -1,28 +1,47 @@
 import random
 
-mitu_mängijat = input('Sisesta mängijate arv: ')
-#genereeri igale mängijale nupp ja oma muutuja
-for mängija in range(1, mitu_mängijat + 1):
-    player[mängija] = 0
     
 
 
 
 
 
+def challenges(fail):
+    mitu_rida = 0
+    andmed = []
+    f = open(fail, encoding='UTF-8')
+    for rida in f:
+        uus = rida.strip().split(': ')
+        mitu_rida += 1
+        andmed.append([int(uus[0]),uus[1]])
+    f.close()
+    number = random.randint(1, mitu_rida)
+    print(number)
+    for paar in andmed:
+        if number == paar[0]:
+            küsimus = paar[1]
+    return küsimus
 
-mitu_rida = 0
-andmed = []
-f = open(fail, encoding='UTF-8')
-for rida in f:
-    uus = rida.strip().split(': ')
-    mitu_rida += 1
-    andmed.append([int(uus[0]),uus[1]])
-f.close()
-number = random.randint(1, mitu_rida)
-print(number)
-for paar in andmed:
-    if number == paar[0]:
-        print(paar[1])
+def tiles(position):
+    if position == 1 or position == 10 or position == 16:
+        valjakutse = challenges('truthordrink.txt')
+    elif position == 4 or position == 11 or position == 18:
+        valjakutse = challenges('generalknowledgeq.txt')
+    elif position == 2 or position == 20:
+        print("EVERYBODY DRINKS!")
+    elif position == 3 or position == 9:
+        print("Astu korra seadmega teistest eemale, et järgnevat küsimust näeksid ainult sina!")
+        valjakutse = challenges('paranoia.txt')
+    elif position == 5 or position == 15 or position == 19:
+        valjakutse = challenges('dareordrink.txt')
+    elif position == 6 or position == 14:
+        valjakutse = challenges('neverhaveiever.txt')
+    elif position == 7 or position == 12 or position == 17:
+        valjakutse = challenges('baila.txt')
+    elif position == 8:
+        print("LUCKY YOU! You can rest right now and not drink.")
+    elif position == 13:
+        print("Finish your drink right this second and go make yourself a new one.")
+    return valjakutse
 
 
