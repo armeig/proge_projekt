a48abe8a4e91e1fc49e92e967d7c0389f3b22351
Henri Einroos <henri.einroos@gmail.com>
2023-12-12 12:39:53 +0200

    test

--- tile_testimine.py
+++ tile_testimine.py
@@ -26,28 +26,53 @@ import pygame
 import random
 import sys
 
+import random
 
-
-challenges_dict = {
-    5: [
-        "Slide into your crush’s DMs.",
-        "Tell everybody your biggest secret.",
-        "Tell everybody a secret that you’ve never told anyone.",
-        "Drink! ayyyy ;)",
-        "Tell everyone about your worst date.",
-        "Kiss the person that’s on your left. (If you’re in a relationship with them, kiss the person on your right - if you say no, you drink, if they say no, you both have to drink)",
-        "Compliment the person sitting opposite of you.",
-        "Show us your Instagram search history.",
-        "Show us your Google search history.",
-        "Drink! heh ;)",
-        "Do 150 pushups. (If you start and can’t complete 150 pushups you have to down your drink altogether)",
-        "Give everyone in this room 5€.",
-        "Post on your Insta story “real friends don’t exist, everyone’s fake in this generation”",
-        "Send “i miss you, wanna link?” to your ex (if you don’t have an ex - drink)",
-        "Describe vividly your biggest sexual fantasy.",
-    ],
     
-}
+
+
+
+
+
+def challenges(fail):
+    mitu_rida = 0
+    andmed = []
+    f = open(fail, encoding='UTF-8')
+    for rida in f:
+        uus = rida.strip().split(': ')
+        mitu_rida += 1
+        andmed.append([int(uus[0]),uus[1]])
+    f.close()
+    number = random.randint(1, mitu_rida)
+    print(number)
+    for paar in andmed:
+        if number == paar[0]:
+            küsimus = paar[1]
+    return küsimus
+
+def tiles(position):
+    if position == 1 or position == 10 or position == 16:
+        valjakutse = challenges('truthordrink.txt')
+    elif position == 4 or position == 11 or position == 18:
+        valjakutse = challenges('generalknowledgeq.txt')
+    elif position == 2 or position == 20:
+        print("EVERYBODY DRINKS!")
+    elif position == 3 or position == 9:
+        print("Astu korra seadmega teistest eemale, et järgnevat küsimust näeksid ainult sina!")
+        valjakutse = challenges('paranoia.txt')
+    elif position == 5 or position == 15 or position == 19:
+        valjakutse = challenges('dareordrink.txt')
+    elif position == 6 or position == 14:
+        valjakutse = challenges('neverhaveiever.txt')
+    elif position == 7 or position == 12 or position == 17:
+        valjakutse = challenges('baila.txt')
+    elif position == 8:
+        print("LUCKY YOU! You can rest right now and not drink.")
+    elif position == 13:
+        print("Finish your drink right this second and go make yourself a new one.")
+    return valjakutse
+
+
 
 
 pygame.init()
@@ -132,9 +157,8 @@ def play_game():
                     position += steps
                     dice_rolled = True
 
-                    
-                    if position in challenges_dict:
-                        challenge_text = random.choice(challenges_dict[position])
+                    challenge_text = tiles(position)
+                    if challenge_text:
                         display_popup("Challenge", challenge_text)
                         pygame.display.flip()
                         
